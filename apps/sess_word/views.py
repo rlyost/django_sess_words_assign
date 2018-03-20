32b877b5a59531dc64d38aaf1375a1a7c5d8e830
rlyost <ry@ryost.us>
2018-03-19 19:16:34 -0700

    finished session words assign

--- apps/sess_word/views.py
+++ apps/sess_word/views.py
@@ -5,10 +5,16 @@ def index(request):
     return render(request, 'sess_word/index.html')
 
 def process(request):
-    if request.method == "POST": 
-        request.session['context'] = {"word": request.POST['word'], "word_color": request.POST['word_color'], "word_size": request.POST['word_size'], "time": strftime("%Y-%m-%d %H:%M %p", gmtime())}
+    if 'context' not in request.session:
+        request.session['context'] = ''
+    if request.method == "POST":
+        word = request.POST['word']
+        color = request.POST['word_color']
+        size = request.POST.get('word_size','small')
+        time = strftime("%H:%M %p %B-%d, %Y", gmtime())
+        request.session['context'] += "<br><p class='" +color+ ' ' +size+ "'>"+word+ "</p><p class='add'>  - added at " +time+"</p>"
     return redirect("/", request.session['context'])
 
 def clear(request):
-    del request.session['context']
+    request.session['context'] = ''
     return redirect('/')
